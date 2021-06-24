99effba07829e3b1a0edcc796dea7ba761b0d9d0
Thomas Richter <th.richter@uksh.de>
2021-06-24 14:10:42 +0200

    formatting

--- src/main/java/io/github/dway17/codex_bloom/IBloomHash.java
+++ src/main/java/io/github/dway17/codex_bloom/IBloomHash.java
@@ -6,12 +6,12 @@ import java.util.Date;
 
 public interface IBloomHash {
 
-	String createBase64Result(String firstName, String lastName, char gender, Date bithdate);
+    String createBase64Result(String firstName, String lastName, char gender, Date bithdate);
 
-	String createBase64Result(String firstName, String lastName, char gender, int birthYear, int birthMonth,
-			int birthDay);
+    String createBase64Result(String firstName, String lastName, char gender, int birthYear, int birthMonth,
+	    int birthDay);
 
-	String createBase64Result(String firstName, String lastName, char gender, LocalDate bithdate);
+    String createBase64Result(String firstName, String lastName, char gender, LocalDate bithdate);
 
 
 }
