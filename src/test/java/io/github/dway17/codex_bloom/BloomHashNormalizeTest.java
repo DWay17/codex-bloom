0c209bb47e7edaecafa11a8a1307db90a8a36acd
Thomas Richter <th.richter@uksh.de>
2021-06-24 14:11:29 +0200

    new param for bala sedd

--- src/test/java/io/github/dway17/codex_bloom/BloomHashNormalizeTest.java
+++ src/test/java/io/github/dway17/codex_bloom/BloomHashNormalizeTest.java
@@ -17,57 +17,57 @@ import org.junit.jupiter.params.*;
 
 class BloomHashNormalizeTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+    }
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+    }
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+    @BeforeEach
+    void setUp() throws Exception {
+    }
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+    @AfterEach
+    void tearDown() throws Exception {
+    }
 
-	@Test
-	void testNormalize() {
-		BloomHash bh = new BloomHash(0, 0, 0, 0);
-		String s = "PrOf. aéz";
-		String voc = "AZ";
-		Set<String> remSet = new HashSet();
-		remSet.add("Prof.");
-		LinkedHashMap<String, String> trans = new LinkedHashMap<>();
-		trans.put("é", "e");
-		String actual = bh.normalize(true, s, voc, remSet, trans);
-		String expected = " AEZ";
-		assertEquals(expected, actual);
-	}
+    @Test
+    void testNormalize() {
+	BloomHash bh = new BloomHash(0, 0, 0, 0, 0);
+	String s = "PrOf. aéz";
+	String voc = "AZ";
+	Set<String> remSet = new HashSet();
+	remSet.add("Prof.");
+	LinkedHashMap<String, String> trans = new LinkedHashMap<>();
+	trans.put("é", "e");
+	String actual = bh.normalize(true, s, voc, remSet, trans);
+	String expected = " AEZ";
+	assertEquals(expected, actual);
+    }
 
-	@Test
-	void testNormalizeReplaceNullTransNull() {
-		BloomHash bh = new BloomHash(0, 0, 0, 0);
-		String s = "az";
-		String voc = "AZ";
-		String actual = bh.normalize(true, s, voc, null, null);
-		String expected = "AZ";
-		assertEquals(expected, actual);
-	}
+    @Test
+    void testNormalizeReplaceNullTransNull() {
+	BloomHash bh = new BloomHash(0, 0, 0, 0, 0);
+	String s = "az";
+	String voc = "AZ";
+	String actual = bh.normalize(true, s, voc, null, null);
+	String expected = "AZ";
+	assertEquals(expected, actual);
+    }
 
-	@Test
-	void testNormalizeTransNull() {
-		BloomHash bh = new BloomHash(0, 0, 0, 0);
-		String s = "PrOf. az";
-		String voc = "AZ";
-		Set<String> remSet = new HashSet();
-		remSet.add("Prof.");
- 		String actual = bh.normalize(true, s, voc, remSet, null);
-		String expected = " AZ";
-		assertEquals(expected, actual);
-	}
+    @Test
+    void testNormalizeTransNull() {
+	BloomHash bh = new BloomHash(0, 0, 0, 0, 0);
+	String s = "PrOf. az";
+	String voc = "AZ";
+	Set<String> remSet = new HashSet();
+	remSet.add("Prof.");
+	String actual = bh.normalize(true, s, voc, remSet, null);
+	String expected = " AZ";
+	assertEquals(expected, actual);
+    }
 
 
 }
