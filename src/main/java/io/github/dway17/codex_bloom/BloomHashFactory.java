d6e851ef3b715b3ffc9492bdcdb4bbf7f48aa0de
Thomas Richter <th.richter@uksh.de>
2021-06-25 14:08:44 +0200

    seed from properties because they should keeped secret

--- src/main/java/io/github/dway17/codex_bloom/BloomHashFactory.java
+++ src/main/java/io/github/dway17/codex_bloom/BloomHashFactory.java
@@ -1,11 +1,32 @@
 
 package io.github.dway17.codex_bloom;
 
+import java.io.*;
+import java.util.*;
+
 public class BloomHashFactory {
 
+    static long seedFirstName;
+    static long seedLastName;
+    static long seedBirthdate;
+    static long seedGender;
+    static long seedBalanced;
+
 	public static IBloomHash getDefault() {
-		// TODO Auto-generated method stub
-		return new BloomHash(0, 0, 0, 0);
+	return new BloomHash(seedFirstName, seedLastName, seedBirthdate, seedGender, seedBalanced);
+    }
+
+    static void loadProps() throws IOException {
+	Properties properties = new Properties();
+	ClassLoader loader = Thread.currentThread().getContextClassLoader();
+	InputStream stream = loader.getResourceAsStream("BloomHashFactory.properties");
+	properties.load(stream);
+	seedFirstName = Long.valueOf((String) properties.get("seedFirstName"));
+	seedLastName = Long.valueOf((String) properties.get("seedLastName"));
+	seedBirthdate = Long.valueOf((String) properties.get("seedBirthdate"));
+	seedGender = Long.valueOf((String) properties.get("seedGender"));
+	seedBalanced = Long.valueOf((String) properties.get("seedBalanced"));
+	stream.close();
 	}
 
 
